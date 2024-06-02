2ef66348644af902c3d307c3ad78a5985a83c6c1
Jan Starý <hans@stare.cz>
2024-06-02 18:19:08 +0200

    detect and use sndfile formats
    This includes obsolete formats such as xi
    for the nonexistent FastTracker II.
    
    We should clean that up.

---
+++ test-sndfile.c
@@ -0,0 +1,19 @@
+#include <sndfile.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	SNDFILE* hdl;
+	SF_INFO info;
+
+	info.format = SF_FORMAT_PAF | SF_FORMAT_PCM_16;
+	info.samplerate = 48000;
+	info.channels = 1;
+
+	if ((hdl = sf_open("file.paf", SFM_WRITE, &info)) == NULL)
+		return 1;
+
+	unlink("file.paf");
+	return 0;
+}
