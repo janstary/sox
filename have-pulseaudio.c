0c41bdc065b748d0f6eb1027ba73b39194ac6c7e
Jan Starý <hans@stare.cz>
2024-06-08 10:07:43 +0200

    detect and use pulseaudio

---
+++ have-pulseaudio.c
@@ -0,0 +1,24 @@
+#include <pulse/simple.h>
+#include <stdio.h>
+
+int
+main(void)
+{
+	pa_simple *pa;
+	pa_stream_direction_t dir;
+	pa_sample_spec spec;
+	int error;
+
+	spec.rate = 48000;
+	spec.channels = 2;
+	spec.format = PA_SAMPLE_S32NE;
+	dir = PA_STREAM_PLAYBACK;
+
+	if ((pa = pa_simple_new(
+	NULL, "SoX", dir, NULL, "have-pulse", &spec, NULL, NULL, &error)) == NULL)
+		return 1;
+
+	pa_simple_free(pa);
+
+	return 0;
+}
